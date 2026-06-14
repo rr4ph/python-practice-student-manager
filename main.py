3590f62656b14dcc6124226390794ec8380c0af4
Mykyta Podoltsev <den00571@gmail.com>
2026-06-14 04:19:30 +0100

    fixed the inconsistencies

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-def listStudents():
+def listStudents(names):
     for index, name in enumerate (names, start=1):
         print(f"{index}: {name}")
 
 def addStudent(file, name):
     file.write(f"\n{name}")
 
-def findStudent(list, name):
-    for search in list:
+def findStudent(names, name):
+    for search in names:
         if (search.strip() == name):
             print("Found")
             break
     else:
         print("Not found")
 
-def removeStudent(file, nameDelete):
+def removeStudent(file, names, nameDelete):
     for index, name in enumerate(names):
         if (name.strip() == nameDelete):
             names.pop(index)
