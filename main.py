f073fb57648548354c3ed5deb79e0225d8189470
Mykyta Podoltsev <den00571@gmail.com>
2026-06-14 03:52:06 +0100

    created findStudent method

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ def listStudents():
 def addStudent(file, name):
     file.write(f"\n{name}")
 
-
+def findStudent(names, name):
+    for search in names:
+        if (search.strip() == name):
+            print("Found")
+            break
+    else:
+        print("Not found")
 
 
 
