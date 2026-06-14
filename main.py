bab80450a992e8d5bf1f1bc326f75c2c8bc60d88
Mykyta Podoltsev <den00571@gmail.com>
2026-06-14 04:11:26 +0100

    created removeStudent method

--- main.py
+++ main.py
@@ -5,14 +5,25 @@ def listStudents():
 def addStudent(file, name):
     file.write(f"\n{name}")
 
-def findStudent(names, name):
-    for search in names:
+def findStudent(list, name):
+    for search in list:
         if (search.strip() == name):
             print("Found")
             break
     else:
         print("Not found")
 
-
+def removeStudent(file, nameDelete):
+    for index, name in enumerate(names):
+        if (name.strip() == nameDelete):
+            names.pop(index)
+            for name in names:
+                file.write(name)
+            print(f"Name {nameDelete} has been deleted.")
+            break
+    else:
+        for name in names:
+                file.write(name)
+        print("There is no such name in the file.")
 
 
