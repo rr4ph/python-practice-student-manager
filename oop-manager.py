4a8c9d0facc3293d6c6adf333f3daa38b763aeff
Mykyta Podoltsev <den00571@gmail.com>
2026-06-15 02:03:52 +0100

    Fixed bug in updateFile for writing names in txt

--- oop-manager.py
+++ oop-manager.py
@@ -3,11 +3,13 @@ class studentManager:
         self.filename = "students.txt"
         
         with open(self.filename) as file:
-            self.names = file.readlines()
+            self.names = [name.strip() for name in file.readlines()]
+            print(self.names)
     
     def updateFile(self):
         with open(self.filename, "w") as file:
-            file.write(self.names)
+            for name in self.names:
+                file.write(name + "\n")
     
     def listStudents(self):
         for index, name in enumerate(self.names, start=1):
